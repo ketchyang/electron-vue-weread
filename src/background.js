e74b068ce4583e58157d3b3abfbfd4908aba17f1
ketchyang <ketchyang@outlook.com>
2020-12-29 19:37:00 +0800

    sidebar

--- src/background.js
+++ src/background.js
@@ -1,69 +1,97 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, BrowserView } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
-import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
 ]);
 
-async function createWindow() {
-  // Create the browser window.
+const Store = require("electron-store");
+const store = new Store();
+
+async function createBrowserWindow() {
+  var windowSize = store.get("window-size");
+  var windowWitdh = 1000;
+  var windowHeight = 750;
+  if (windowSize) {
+    console.log("2");
+    console.log(windowSize);
+    windowWitdh = windowSize.width;
+    windowHeight = windowSize.height;
+  } else {
+    console.log("1"); 
+    console.log(windowSize);
+  }
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: windowWitdh,
+    height: windowHeight,
+    minWidth: 1000,
+    minHeight: 750,
+    backgroundColor: "#000",
+    show: false,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
     }
   });
 
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+
+  win.on("will-resize", (event, newBounds) => {
+    store.set("window-size", {
+      width: newBounds.width,
+      height: newBounds.height
+    });
+  });
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
     createProtocol("app");
-    // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
+
+  return win;
 }
 
-// Quit when all windows are closed.
+function createBrowserView(win) {
+  const view = new BrowserView({});
+  win.setBrowserView(view);
+  view.setBounds({
+    x: 180,
+    y: 0,
+    width: win.getBounds().width - 180,
+    height: win.getBounds().height
+  });
+  view.setAutoResize({
+    width: true,
+    height: true
+  });
+  view.webContents.loadURL("https://weread.qq.com");
+}
+
+function createBrowserWindowAndView() {
+  createBrowserWindow().then(win => createBrowserView(win));
+}
+
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) createBrowserWindowAndView();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS);
-    } catch (e) {
-      console.error("Vue Devtools failed to install:", e.toString());
-    }
-  }
-  createWindow();
+  createBrowserWindowAndView();
 });
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === "win32") {
     process.on("message", data => {
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="side-bar">
+    <span class="side-bar-item" >推荐</span>
+    <span class="side-bar-item">我的书架</span>
+    <span class="side-bar-item">文档上传</span>
+    <span class="side-bar-item">正在阅读</span>
+    <span class="side-bar-item">设置</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="scss">
+.side-bar {
+  width: 200px;
+  height: 100vh;
+  background: #606266;
+
+  .side-bar-item {
+    height: 50px;
+    display: block;
+    line-height: 50px;
+    font-size: 18px;
+    padding-left: 30px;
+    color: white;
+  }
+
+  .side-bar-item:hover {
+    background: #909399;
+    -moz-transition: all 1s ease 0s;
+    -webkit-transition: all 1s ease 0s;
+    transition: all 1s ease 0s;
+    -o-transition: all 1s ease 0s;
+  }
+}
+</style>
